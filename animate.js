9432105cd39b44f9481d3b75ef4f6abbbef6543e
Kakashi68Hatake <pdoriya68@gmail.com>
2019-09-30 23:30:17 +0530

    first commit

---
+++ animate.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 650) {
+        $("#navbar").slideDown('slow');
+      } else {
+        $("#navbar").slideUp('slow');
+      }
+
+      if ($(document).scrollTop() > 100) {
+        $("#aboutme").fadeOut(1000);
+      } else {
+        $("#aboutme").fadeIn(1000);
+      }
+
+      if ($(document).scrollTop() > 1300) {
+        $(".skillset2").show(800);
+        $(".skillset1").show(1000);
+        $('.inbar div').delay(750).show(1000);
+      } else {
+        $(".skillset2").hide(800);
+        $(".skillset1").hide(1000);
+        $('.inbar div').hide();
+      } 
+    });
+  });
+  
